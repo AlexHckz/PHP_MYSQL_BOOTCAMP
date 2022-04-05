a48ece11f5bd6932a87a6be1a4ee83263ac8e58f
Alexandre Cuneo <alexandre.cuneo@gmail.com>
2022-04-05 11:19:15 +0200

    Touche_Coule

--- functions_challenges/functions_challenges.php
+++ functions_challenges/functions_challenges.php
@@ -6,7 +6,7 @@ function compterCaracteres(string $chaineCaractere)
     $nbCaracteres = strlen($chaineCaractere);
     return $nbCaracteres;
 }
-echo "Le nombre de caracères de ma chaine est " . compterCaracteres("undeuxtrois");
+//echo "Le nombre de caracères de ma chaine est " . compterCaracteres("undeuxtrois");
 
 
 
@@ -20,4 +20,30 @@ function miseEnformeChaine(string $chaineCaractere)
     return $miseEnForme;
 }
 
-echo "Ma chaîne mise en forme retourne " . miseEnformeChaine("TesT DE miSe En FoRMe");
+//echo "Ma chaîne mise en forme retourne " . miseEnformeChaine("TesT DE miSe En FoRMe");
+
+//Exo 3 - Fonction Touché Coulé 
+
+
+function toucheCoule(string $coordX, string $coordY)
+{
+    $coord = $coordX . $coordY;
+    $patternX = "#^[A-J]$#";
+    $patternY = '#^[0-9]$#';
+    $testX = preg_match($patternX, $coordX);
+    $testY = preg_match($patternY, $coordY);
+    $touche = ["F3", "F4", "F5", "I5", "I6", "C9", "D9", "E9", "F9"];
+    $testTouche = in_array($coord, $touche);
+
+    if (!$testX || !$testY) {
+        return "hors-jeu!";
+    } elseif ($testX && $testY) {
+        if ($testTouche) {
+            return "touché!\n";
+        } else {
+            return "coulé!\n";
+        }
+    }
+}
+
+echo toucheCoule("Z", 35);
