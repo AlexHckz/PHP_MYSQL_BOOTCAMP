06b20015054e704d9e9fde97661d5cd366f0b9d5
Alexandre Cuneo <alexandre.cuneo@gmail.com>
2022-04-07 10:58:24 +0200

    mise a jour etat

---
+++ exercice_git/functions.php
@@ -0,0 +1,17 @@
+<?php
+
+
+function nbEntier()
+{
+    do {
+        $tmp = readline("Entez un nombre entier plus grand que 0 >>");
+    } while (!ctype_digit($tmp));
+
+    if ($tmp <= 1) {
+        return 1;
+    } else {
+        return $tmp;
+    }
+}
+
+echo nbEntier();
